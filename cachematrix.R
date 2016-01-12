bc95baaaef447f347c3b7a89d1d640ce00924334
cfroccajr <cfroccajr@gmail.com>
2016-01-12 16:23:31 -0500

    Comments and functions have been fleshed out according to specifications.
    I inserted appropriate comments and fleshed out the functions so that
    they performed according to given requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script consits of two functions: 
+## The first will create a list using a matrix.  This list will keep track of 
+## the matrix and its inverse.
+## The second retrieves the inverse of the matrix in the object created by 
+## the first function, calculating it if necessary.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of four functions:
+## set <- sets the value of the matrix x and resets inv_x to matrix()
+## set_inv <- sets the value of inv_x to the inverse of x
+## get <- retrieves the value of x
+## get_inv <- retrievs the value of x_inv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_x <- matrix()
+        
+        set <- function(y){
+                x <<- y
+                inv_x <<- matrix()
+        }
+        
+        set_inv <- function() inv_x <<- solve(x)
+        
+        get <- function() x
+        
+        get_inv <- function() inv_x
+        
+        list(set = set, 
+             set_inv = set_inv,
+             get = get,
+             get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in a makeCacheMatrix list x and returns the inverse.
+## If the inverse has already been calculated then it returns the cached version.
+## If it has not been cached then it sets the inverse and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$get_inv()
+        
+        if(!identical(x$get_inv(),matrix())){
+                message("Here is the cached inverse:")
+                return(inv)
+        }
+        x$set_inv()
+        inv <- x$get_inv()
+        message("Here is the un-cached inverse:")
+        inv
+        
 }
